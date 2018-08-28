f47690e2ecfabe29a31c5c9467feaebc405f06c2
Daniel Pieta <daniel177p@gmail.com>
2018-08-28 13:19:13 +0200

    Modify seed for employees

--- MVCTreeview/Migrations/EmployeeMigrations/Configuration.cs
+++ MVCTreeview/Migrations/EmployeeMigrations/Configuration.cs
@@ -16,23 +16,21 @@ namespace MVCTreeview.Migrations.EmployeeMigrations
 
         protected override void Seed(MVCTreeview.context.EmployeeContext context)
         {
-            //Not working!
-
-            //context.Employees.AddOrUpdate(
-            //  p => p.EmployeeName,
-            //  new Employee { EmployeeID = 1, EmployeeName = "Root"},
-            //  new Employee { EmployeeID = 2, EmployeeName = "John",   ManagerID = 1 },
-            //  new Employee { EmployeeID = 3, EmployeeName = "Thomas", ManagerID = 4 },
-            //  new Employee { EmployeeID = 4, EmployeeName = "Eric",   ManagerID = 1 },
-            //  new Employee { EmployeeID = 5, EmployeeName = "Jessica", ManagerID = 4 },
-            //  new Employee { EmployeeID = 6, EmployeeName = "Kate",    ManagerID = 2 },
-            //  new Employee { EmployeeID = 7, EmployeeName = "Bob",    ManagerID = 6 },
-            //  new Employee { EmployeeID = 8, EmployeeName = "Steven", ManagerID = 7 },
-            //  new Employee { EmployeeID = 9, EmployeeName = "Joanna", ManagerID = 7 },
-            //  new Employee { EmployeeID = 10, EmployeeName = "Tim",    ManagerID = 2 },
-            //  new Employee { EmployeeID = 11, EmployeeName = "Rebeca", ManagerID = 2 },
-            //  new Employee { EmployeeID = 12, EmployeeName = "Peter",  ManagerID = 8 }
-            //);
+            context.Employees.AddOrUpdate(
+              p => p.EmployeeName,
+              new Employee { EmployeeID = 1, EmployeeName = "Root", ManagerID = null },
+              new Employee { EmployeeID = 2, EmployeeName = "John", ManagerID = 1 },
+              new Employee { EmployeeID = 3, EmployeeName = "Thomas", ManagerID = 4 },
+              new Employee { EmployeeID = 4, EmployeeName = "Eric", ManagerID = 1 },
+              new Employee { EmployeeID = 5, EmployeeName = "Jessica", ManagerID = 4 },
+              new Employee { EmployeeID = 6, EmployeeName = "Kate", ManagerID = 2 },
+              new Employee { EmployeeID = 7, EmployeeName = "Bob", ManagerID = 6 },
+              new Employee { EmployeeID = 8, EmployeeName = "Steven", ManagerID = 7 },
+              new Employee { EmployeeID = 9, EmployeeName = "Joanna", ManagerID = 7 },
+              new Employee { EmployeeID = 10, EmployeeName = "Tim", ManagerID = 2 },
+              new Employee { EmployeeID = 11, EmployeeName = "Rebeca", ManagerID = 2 },
+              new Employee { EmployeeID = 12, EmployeeName = "Peter", ManagerID = 8 }
+            );
         }
     }
 }
